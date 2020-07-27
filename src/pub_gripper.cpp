1b56474003452e297ee56e60105260ef1ebf36cf
RX-00 <00royxing@gmail.com>
2020-07-27 18:48:23 -0400

    I need to fix this git structure

--- src/pub_gripper.cpp
+++ src/pub_gripper.cpp
@@ -58,7 +58,7 @@ int main(int argc, char **argv)
     std_msgs::String msg;
 
     std::stringstream ss;
-    ss << "open" << count;
+    ss << "open " << count;
     msg.data = ss.str();
 
     ROS_INFO("%s", msg.data.c_str());
